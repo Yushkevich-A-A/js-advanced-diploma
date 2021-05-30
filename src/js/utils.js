153386d7c7ea80ddf3b7af9134c6c2b2bd6732e1
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-05-30 21:41:44 +0300

    field borders drew

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,20 @@
 export function calcTileType(index, boardSize) {
+  const amountOfCells = boardSize ** 2;
+  const topLeftCell = 0;
+  const topRightCell = boardSize - 1;
+  const bottomLeftCell = amountOfCells - boardSize;
+  const bottomRightCell = amountOfCells - 1;
+
   // TODO: write logic here
-  return 'center';
+  return (index === topLeftCell) ? 'top-left':
+        (index === topRightCell) ? 'top-right':
+        (index > topLeftCell && index < topRightCell) ? 'top':
+        (index === bottomLeftCell) ? 'bottom-left':
+        (index === bottomRightCell) ? 'bottom-right':
+        (index > bottomLeftCell && index < bottomRightCell) ? 'bottom':
+        (index % boardSize - topRightCell === 0) ? 'right':
+        (index % boardSize === 0) ? 'left':
+        'center';
 }
 
 export function calcHealthLevel(health) {
